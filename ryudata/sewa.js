0f0cfe256c7dadad00d26e0de56bd4eb88b8b03f
OzywDsni <103554187+OzywDsni@users.noreply.github.com>
2022-04-29 13:46:12 +0700

    Update sewa.js

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -1,10 +1,10 @@
 exports.sewa = () => {
 	return`𝗢 𝗣 𝗘 𝗡  𝗦 𝗘 𝗪 𝗔  𝗕 𝗢 𝗧
 
-• *1 Minggu : 5.000 ( 5K )*
-• *1 Bulan : 10.000 ( 10K )*
-• *Permanen : 15.000 ( 15K )*
-• *Join Murid Bot : 20.000 ( 20K )*
+• *Permanen : 8.000 ( 8K )*
+• *Permanen : 8.000 ( 8K )*
+• *Permanen : 8.000 ( 8K )*
+• *Join Murid Bot : 50.000 ( 50K )*
 
 Minat Ketik .owner
 `
