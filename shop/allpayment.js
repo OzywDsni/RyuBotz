cf822af3c6686f7d42d8b79a23990d4f14eb83db
OzywDsni <103554187+OzywDsni@users.noreply.github.com>
2022-04-30 01:26:45 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -5,15 +5,15 @@ return `┏━━━━━━━━━━━━━━━━━━━━
 ┣━━⊱❉ *💰 E-WALLET* ❉⊰━━━✿
 ┃   
 ┣━⊱ *GOPAY*
-┣⊱ 0899 0000 0000
+┣⊱ 0856 4981 2379
 ┣━━✿ NAMA GOPAY ${ownername}
 ┃
 ┣━⊱ *DANA*
-┣⊱ 0899 0000 0000
+┣⊱ CLOSE
 ┣━━✿ NAMA DANA ${ownername}
 ┃
 ┣━⊱ *PULSA*
-┣⊱ 0899 0000 0000
+┣⊱ 0856 4981 2379
 ┣━━✿ NAMA PULSA ${ownername}
 ┃
 ┗━━━━━━━━━━━━━━━━━━━━
